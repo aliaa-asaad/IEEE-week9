09ee8234d79dde7fc8b8c1f01a04eee9fe9517df
Aliaa Asaad <aliaaasaad35@gmail.com>
2022-09-12 20:58:05 +0200

    second commit

--- lib/model/body.dart
+++ lib/model/body.dart
@@ -9,15 +9,16 @@ class Body {
     'Underweight': 'You have a Underweight body weight.\nEat some food',
     'Overweight': 'You have a Overweight body weight.\nGo to Gym',
   };
+
 bmi(height, weight)=>((weight!*10000) / (height! * height!)).round();
   bmiMessage(height, weight) {
     double bmi = (weight!*10000) / (height! * height!);
     if (bmi < 18.5)
-      return message[1];
+      return message.keys.elementAt(1);
     else if (bmi >= 18.5 || bmi <= 24.9)
-      return message[0];
+      return message.keys.elementAt(0);
     else
-      return message[2];
+      return message.keys.elementAt(2);
   }
 
   bmiColor(height, weight) {
